8debba9e7a0a9c29358e437f7f9d4236a8779e41
Moataz Wesimy <wesimy@gmail.com>
2018-02-23 01:51:03 +0400

    home page

--- Source/www/src/components/formGetStarted/FormGetStarted.js
+++ Source/www/src/components/formGetStarted/FormGetStarted.js
@@ -4,22 +4,77 @@ class FormGetStarted extends Component {
   render() {
     return (
      <div className="form-get-started">
-        <ul data-uk-accordion="toggle: > .uk-accordion-t ">
-            
-            <li>
-                <a className="uk-accordion-t uk-button uk-button-secondary" href="#">Sign in</a>
-                <div className="uk-accordion-content">
-                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
-                </div>
-            </li>
-            <li>
-                
-                <a className="uk-accordion-t uk-button uk-button-primary" href="#">Get Started</a>
-                <div className="uk-accordion-content">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-            </li>
-        </ul>
+     
+            <ul data-uk-accordion="toggle: > .uk-accordion-tab">
+               
+                <li>
+                    <a className="uk-accordion-tab uk-button uk-button-secondary uk-width-1-1"  href="#">Get Started</a>
+                    <div className="uk-accordion-content">
+                    <form>
+
+<div >
+    <div className="uk-inline uk-width-1-1">
+        <input placeholder="Email" className="uk-input uk-form-blank" type="text"/>
+    </div>
+</div>
+
+<div>
+    <div className="uk-inline uk-width-1-1">
+        <input placeholder="Username" className="uk-input uk-form-blank" type="text"/>
+    </div>
+</div>
+
+<div >
+    <div className="uk-inline uk-width-1-1">
+        
+        <input placeholder="Password" className="uk-input uk-form-blank" type="password"/>
+    </div>
+</div>
+
+<div className="uk-margin">
+    <div className="uk-inline uk-width-1-1">
+        <label><input className="uk-checkbox" type="checkbox"/> <span className="uk-text-meta uk-text-left">I have read, understand, and agree to the honestgram Terms of Service.</span></label>
+    </div>
+</div>
+
+<div className="uk-margin-small">
+
+<button className="uk-button uk-button-secondary uk-width-1-1">Sign up</button>
+
+</div>
+
+</form>
+                    </div>
+                </li>
+
+                 <li>
+                    <a className="uk-accordion-tab uk-button uk-button-third uk-width-1-1" href="#">Sign in</a>
+                    <div className="uk-accordion-content">                        
+                    <form>
+                        <div className="uk-margin-small">
+                            <div className="uk-inline uk-width-1-1">
+                                <span className="uk-form-icon" uk-icon="icon: user"></span>
+                                <input placeholder="email" className="uk-input uk-form-blank" type="text"/>
+                            </div>
+                        </div>
+
+                        <div className="uk-margin-small">
+                            <div className="uk-inline uk-width-1-1">
+                                <span className="uk-form-icon" uk-icon="icon: lock"></span>
+                                <input placeholder="password" className="uk-input uk-form-blank" type="password"/>
+                            </div>
+                        </div>
+                        <div className="uk-margin-small">
+                        <button className="uk-button  uk-button-third uk-width-1-1">Sign in</button>
+                        </div>
+
+                        </form>
+
+                    </div>
+                </li>
+            </ul>
+       
+        
      </div>
     )
   }
--- Source/www/src/components/homeSlideshow/HomeSlideShow.js
+++ Source/www/src/components/homeSlideshow/HomeSlideShow.js
@@ -1,55 +1,105 @@
 import React, { Component } from 'react';
-import './homeSlideShow.scss';
+import TypeIt from 'typeit';
 import FormGetStarted from '../formGetStarted/FormGetStarted';
 import * as Spread from '../../components/spread/Spread';
+import logo from '../../assets/images/paper-plane.svg';
+import speach from '../../assets/images/paper-speach.svg';
+import UIkit from 'uikit';
+
+import './homeSlideShow.scss';
 
 class HomeSlideShow extends Component {
     constructor() {
         super();
-        this.state = {currentSlide: 0};
+        this.state = {currentSlide: 0, sliding: false};
     }
     componentDidMount() {
         document.querySelector('#home-slide-show').addEventListener('beforeitemshow',(event)=>{
           if(event.detail[0].index !== this.state.currentSlide){
             Spread.start(event.target.getAttribute('data-background'));
             this.setState({
-                currentSlide: event.detail[0].index
+                currentSlide: event.detail[0].index,
+                sliding: true
               });
+              setTimeout(()=>{
+                this.setState({
+                              sliding: false
+                            });
+            },2000);
           }
         });
+        // typit
+        new TypeIt('.type-it', {
+            strings: ['a twat', 'a sweatheart', 'a bumm', 'a cool guy', 'an idiot', 'a dick'],
+            loop: true,
+            nextStringDelay: 4000,
+            breakLines: false,
+       });
     }
+    wheel(e) {
+        if(!this.state.sliding){
+            if(e.deltaY < 0 & this.state.currentSlide > 0){
+              
+                UIkit.slideshow(document.querySelector('#home-slide-show')).show(this.state.currentSlide - 1);
+            }
+            else if(e.deltaY > 0 & this.state.currentSlide < 4){
+               
+                UIkit.slideshow(document.querySelector('#home-slide-show')).show(this.state.currentSlide + 1);
+            }
+            
+        }
+        
+        
+      }
     render() {
         return (
-            <div id="home-slide-show" className="home-slide-show uk-position-relative uk-visible-toggle" data-uk-slideshow="animation: fade" >
+            <div id="home-slide-show" className="home-slide-show uk-position-relative uk-visible-toggle" data-uk-slideshow="animation: fade"  onWheel = {(e) => this.wheel(e)}>
                 <ul className="uk-slideshow-items" data-uk-height-viewport>
-                    <li data-background="#fff">
-                        <div className="uk-position-center uk-position-small uk-text-center uk-transition-fade">
-                            <div className="brand">
-                                <h1 title="honestgram" data-uk-slideshow-parallax="y: -60,60">honestgram</h1>
+                    <li className="home-intro" data-background="#f6f6f6">
+                        <div className="uk-width-4-5@l uk-width-3-5@xl uk-position-center uk-position-small uk-text-center uk-transition-fade">
+                        <div className="uk-container">
+                        <div className="brand">
+                                
+                                <h1 title="honestgram" data-uk-slideshow-parallax="y: -60,60">honestgram<img src={logo} alt="honestgram logo"/></h1>
                             </div>
                             <p data-uk-slideshow-parallax="y: -50,50">speak your mind, stop the gossips</p>
-                            <section>
-                            <FormGetStarted/>
-                            </section>
+                            
+                                <div className="uk-flex-center uk-grid-collapse" data-uk-grid>
+                                    <div className="uk-width-1-1 uk-width-1-3@m uk-width-1-4@xl">
+                                    <FormGetStarted/>
+                                    </div>
+                                </div>
+                
+                        </div>
+                            
                         </div>
                     </li>
-                    <li data-background="#9c342e">
-                        <div className="uk-position-center uk-position-small uk-text-center uk-transition-fade">
-                            <h2 data-uk-slideshow-parallax="y: -60,60">Heading</h2>
-                            <p  data-uk-slideshow-parallax="y: -50,50">Lorem ipsum dolor sit amet.</p>
+                    <li className="uk-light home-slide" data-background="#9c342e">
+                        <div className="uk-width-4-5@l uk-width-3-5@xl uk-position-center uk-position-small uk-text-center uk-transition-fade">
+                        <div className="uk-container">
+                        
+                        <h2><b>honestgram</b> is a place for everyone to tell you<br/>what<span className="type-it"></span>you are</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                       
+                        </div>
                         </div>
                     </li>
-                    <li data-background="#fad251">
-                        <div className="uk-position-center uk-position-small uk-text-center uk-transition-fade">
-                            <h2  data-uk-slideshow-parallax="y: -60,60">Heading</h2>
-                            <p  data-uk-slideshow-parallax="y: -50,50">Lorem ipsum dolor sit amet.</p>
+                    <li className="uk-light home-slide" data-background="#fad251">
+                        <div className="uk-width-4-5@l uk-width-3-5@xl uk-position-center uk-position-small uk-text-center uk-transition-fade">
+                        <div className="uk-container">
+                            <div className="home-slide-02">
+                            <img className="img-speach" src={speach} alt="honestgram"/>
+                            </div>
+                        </div>
                         </div>
                     </li>
-                    <li data-background="#fff">
-                        <div className="uk-position-center uk-position-small uk-text-center uk-transition-fade">
+                    <li className="home-intro" data-background="#f6f6f6">
+                        <div className="uk-width-4-5@l uk-width-3-5@xl uk-position-center uk-position-small uk-text-center uk-transition-fade">
+                        <div className="uk-container">
                             <h2  data-uk-slideshow-parallax="y: -60,60">Lets Get Started</h2>
                             <p  data-uk-slideshow-parallax="y: -50,50">Lorem ipsum dolor sit amet.</p>
                             <FormGetStarted/>
+                        </div>
                         </div>
                     </li>
                 </ul>
